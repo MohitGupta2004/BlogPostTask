7c3f9b0fd5cfcbb6b04fba31b48eca48cf0d9be0
MohitGupta2004 <mohit0907gupta@gmail.com>
2025-09-13 17:46:40 +0530

    first commit

---
+++ src/main/java/com/ancilar_tasks/backendtask/Controller/CommentsController.java
@@ -0,0 +1,42 @@
+package com.ancilar_tasks.backendtask.Controller;
+
+import com.ancilar_tasks.backendtask.Service.CommentsService;
+import com.ancilar_tasks.backendtask.model.Comments;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+//CommentsController
+@RestController
+public class CommentsController {
+
+    private final CommentsService commentsService;
+
+    @Autowired
+    public CommentsController(CommentsService commentsService) {
+        this.commentsService = commentsService;
+    }
+
+    //POST /posts/:id/comments: Add a comment to a post.
+    //GET /posts/:id/comments: List comments for a post.
+    //DELETE /comments/:id: Delete a specific comment.
+
+    @PostMapping("/posts")
+    public void addCommentToPost(@RequestBody Comments comment) {
+        commentsService.addCommentToPost(comment);
+    }
+
+    @GetMapping("/posts/{postId}/comments")
+    public List<Comments> listCommentsForPost(@PathVariable Long postId) {
+        return commentsService.getCommentsForPost(postId);
+    }
+
+    @DeleteMapping("/comments/{commentId}")
+    public void deleteComment(@PathVariable Long commentId) {
+        commentsService.deleteCommentById(commentId);
+    }
+
+
+
+
+}
